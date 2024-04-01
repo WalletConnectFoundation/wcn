fc3aa544bb1a296d185cec1c65f6e9b4308321c6
Darksome <dev@darkso.me>
2024-04-01 09:18:57 +0000

    chore: remove terraform/monitoring

--- terraform/monitoring/data_sources.tf
+++
@@ -1,9 +0,0 @@
-data "grafana_data_source" "prometheus" {
-  name = "${var.environment}-relay-amp"
-}
-
-
-data "grafana_data_source" "cloudwatch-eu" {
-  name = "${var.environment}-relay-cloudwatch"
-}
-

7d8a81bd669e786b6db2ad5f910144b0b2d0c3c4
Ivan Reshetnikov <keenn2007@gmail.com>
2024-05-13 15:38:26 +0200

    feat: CLI key generation (#24)

--- crates/irn/src/commands/config.rs
+++ crates/irn/src/commands/config.rs
@@ -1,7 +1,6 @@
 #[derive(Debug, clap::Args)]
 pub struct ConfigCmd;
 
-#[tracing::instrument(skip(_args))]
 pub fn exec(_args: ConfigCmd) -> anyhow::Result<()> {
     Ok(())
 }
--- crates/irn/src/commands/node/node_config.rs
+++ crates/irn/src/commands/node/node_config.rs
@@ -1,7 +1,6 @@
 #[derive(Debug, clap::Args)]
 pub struct NodeConfigCmd;
 
-#[tracing::instrument(skip(_args))]
 pub fn exec(_args: NodeConfigCmd) -> anyhow::Result<()> {
     Ok(())
 }

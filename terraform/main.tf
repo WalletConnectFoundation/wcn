dda19beb8379d04ced6952a7454b3dcbee869e1f
Darksome <dev@darkso.me>
2024-05-17 14:08:53 +0000

    bump image version

--- terraform/main.tf
+++ terraform/main.tf
@@ -136,7 +136,7 @@ module "node" {
   region      = "eu-central-1"
   id          = each.key
   environment = local.environment
-  image       = "${data.aws_ecr_repository.node.repository_url}:pr-32"
+  image       = "${data.aws_ecr_repository.node.repository_url}:pr-33"
   node_memory = 4096 - 512
   node_cpu    = 2048
 

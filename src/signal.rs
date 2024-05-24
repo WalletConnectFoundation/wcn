bde488c5cda97fb3ba65c22890a19b13c37ea215
Ivan Reshetnikov <keenn2007@gmail.com>
2024-05-24 13:14:35 +0200

    feat: restart on sigint

--- src/signal.rs
+++ src/signal.rs
@@ -2,21 +2,19 @@ use {
     anyhow::Context as _,
     futures::Future,
     irn::ShutdownReason,
-    std::pin::pin,
     tokio::signal::unix::{self, Signal, SignalKind},
 };
 
 pub fn shutdown_listener() -> anyhow::Result<impl Future<Output = ShutdownReason>> {
+    let mut sigint = listener(SignalKind::interrupt())?;
     let mut sigterm = listener(SignalKind::terminate())?;
     let mut sigusr1 = listener(SignalKind::user_defined1())?;
 
     Ok(async move {
-        let mut sigterm = pin!(sigterm.recv());
-        let mut sigusr1 = pin!(sigusr1.recv());
-
         tokio::select! {
-            _ = &mut sigterm => ShutdownReason::Restart,
-            _ = &mut sigusr1 => ShutdownReason::Decommission,
+            _ = sigint.recv() => ShutdownReason::Restart,
+            _ = sigterm.recv() => ShutdownReason::Restart,
+            _ = sigusr1.recv() => ShutdownReason::Decommission,
         }
     })
 }

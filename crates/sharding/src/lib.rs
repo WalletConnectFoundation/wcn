d58b3ff3faefa3904aa1fa1dcd66474da48e425c
Darksome <dev@darkso.me>
2024-07-24 12:18:51 +0000

    WIP

--- crates/sharding/src/lib.rs
+++ crates/sharding/src/lib.rs
@@ -7,6 +7,7 @@ use {
         char::MAX,
         collections::{BTreeSet, HashMap, HashSet},
         default,
+        fmt,
         hash::{DefaultHasher, Hash, Hasher, SipHasher},
         iter,
         marker::PhantomData,
@@ -61,7 +62,7 @@ impl<const RF: usize, N> Keyspace<RF, N> {
     /// Creates a new [`Keyspace`].
     pub fn new(
         nodes: impl IntoIterator<Item = N>,
-        hasher_factory: impl HasherFactory,
+        hasher_factory: &impl HasherFactory,
         mut sharding_strategy: impl Strategy<N>,
     ) -> Result<Self, Error>
     where
@@ -193,7 +194,7 @@ pub enum Error {
     #[error("Replication factor should be in range [{}, {}]", min, max)]
     InvalidReplicationFactor { min: usize, max: usize },
 
-    #[error("Replication factor should be in range [{}, {}]", min, max)]
+    #[error("Nodes count should be in range [{}, {}]", min, max)]
     InvalidNodesCount { min: usize, max: usize },
 
     #[error("Sharding strategy didn't select enough nodes to fill a replica set")]
@@ -203,9 +204,14 @@ pub enum Error {
 #[cfg(any(test, feature = "testing"))]
 impl<const RF: usize, N> Keyspace<RF, N>
 where
-    N: Hash,
+    N: Eq + Hash + fmt::Debug,
 {
-    fn assert_variance(&self, max_deviasion_coefficient: f64) {
+    fn assert_variance_and_stability(
+        &self,
+        old: &Self,
+        expected_variance: &ExpectedDistributionVariance,
+    ) {
+        dbg!(&self.nodes);
         let mut shards_per_node = HashMap::<usize, usize>::new();
 
         for shard in &self.shards {
@@ -224,93 +230,144 @@ where
         let coefficient = max_deviation as f64 / mean as f64;
 
         dbg!(mean, max_deviation, coefficient);
-        assert!(coefficient <= max_deviasion_coefficient);
+
+        let allowed_coefficient = expected_variance.get(self.nodes.len());
+        assert!(coefficient <= allowed_coefficient);
+
+        let old_nodes: HashSet<_> = old.nodes().collect();
+        let new_nodes: HashSet<_> = self.nodes().collect();
+
+        let mut allowed_shard_movements = 0;
+
+        let removed: HashSet<_> = old_nodes.difference(&new_nodes).collect();
+        let added: HashSet<_> = new_nodes.difference(&old_nodes).collect();
+
+        if !removed.is_empty() {
+            for shard in &old.shards {
+                for replica_idx in &shard.replicas {
+                    if removed.contains(&old.nodes.get_index(*replica_idx).unwrap()) {
+                        allowed_shard_movements += 1;
+                    }
+                }
+            }
+        }
+
+        allowed_shard_movements +=
+            (((added.len() * RF * mean) as f64) * (1.0 + allowed_coefficient)) as usize;
+
+        let mut shard_movements = 0;
+        let mut replicas: HashSet<_>;
+        for shard_idx in 0..old.shards.len() {
+            let shard_id = ShardId(shard_idx as u16);
+
+            replicas = self.shard_replicas(shard_id).collect();
+
+            for node_id in old.shard_replicas(shard_id) {
+                if !replicas.contains(node_id) {
+                    shard_movements += 1;
+                }
+            }
+        }
+
+        dbg!(shard_movements, removed, added);
+        assert!(
+            shard_movements <= allowed_shard_movements,
+            "{shard_movements} <= {allowed_shard_movements}"
+        );
     }
-
-    // fn assert_stability(&self, old: &Self) {
-    // let mut n = 0;
-
-    // for (shard_idx, shard) in a.hashring.iter().enumerate() {
-    //     let new_replicas = b.hashring[shard_idx].replicas;
-
-    //     for replica_idx in shard.replicas {
-    //         if !new_replicas.iter().any(|idx| idx == &replica_idx) {
-    //             n += 1;
-    //         }
-    //     }
-
-    //     // if shard.replicas[0] != new_replicas[0] {
-    //     //     n += 1;
-    //     // }
-    // }
-
-    // n
-
-    //     let to_move = shards_to_migrate(old, self);
-    //     dbg!(to_move);
-    // }
 }
 
-fn test_suite() {}
+/// Specifies the expected shard distribution variance.
+///
+/// Each element of the [`Vec`] should be an `(N, C)` tuple, where `N` is number
+/// of nodes in the cluster and `C` is a maximum allowed mean absolute deviation
+/// coefficient.
+///
+/// For example, coefficient of `0.1` means that the maximum and the minimum
+/// amount of shards per node are allowed to deviate from the mean (the average)
+/// by 10%.
+/// Let's say the expected mean is 100 shards per node, then coefficient of
+/// `0.1` will allow the number of shards per node to deviate in the range of
+/// `90..=110`.
+///
+/// Not every cluster size needs to be specified, one may specify
+/// the expected variance as a range:
+/// ```
+/// sharding::ExpectedDistributionVariance(vec![(4, 0.01), (8, 0.02), (256, 0.1)]);
+/// ```
+/// This would mean that for a cluster size of up to 4 nodes the coefficient is
+/// `0.01`, for a cluster size in the range of `5..8` the coefficient is `0.02`
+/// and so on.
+pub struct ExpectedDistributionVariance(pub Vec<(usize, f64)>);
 
-#[test]
-fn t() {
-    let hasher = || DefaultHasher::new();
-    let strategy = DefaultStrategy;
+impl ExpectedDistributionVariance {
+    fn get(&self, nodes_count: usize) -> f64 {
+        for (count, coef) in self.0.iter().copied() {
+            if nodes_count <= count {
+                return coef;
+            }
+        }
 
-    let mut keyspace: Keyspace<3, _> = Keyspace::new([1, 2, 3], hasher, strategy).unwrap();
+        panic!("Missing coefficient for nodes_count: {nodes_count}");
+    }
+}
 
-    for id in 4..=16 {
-        let nodes = keyspace.nodes().cloned().chain([id]);
-        let new_keyspace = Keyspace::new(nodes, hasher, strategy).unwrap();
-        new_keyspace.assert_variance(0.03);
+#[cfg(any(test, feature = "testing"))]
+pub fn test_suite<const RF: usize, NodeId, S>(
+    hasher_factory: impl HasherFactory,
+    sharding_strategy_factory: impl FnMut() -> S,
+    node_id_factory: impl FnMut() -> NodeId,
+    expected_variance: ExpectedDistributionVariance,
+) where
+    NodeId: Eq + Hash + Ord + Clone + fmt::Debug,
+    S: Strategy<NodeId>,
+{
+    use rand::random;
+
+    let new_hasher = hasher_factory;
+    let mut new_strategy = sharding_strategy_factory;
+    let mut new_node_id = node_id_factory;
+
+    let initial_nodes = (0..RF).map(|_| new_node_id());
+
+    let mut keyspace: Keyspace<RF, _> =
+        Keyspace::new(initial_nodes, &new_hasher, new_strategy()).unwrap();
+    keyspace.assert_variance_and_stability(&keyspace, &expected_variance);
+
+    for _ in 0..10 {
+        let nodes = keyspace.nodes().cloned().chain([new_node_id()]);
+        let new_keyspace = Keyspace::new(nodes, &new_hasher, new_strategy()).unwrap();
+        new_keyspace.assert_variance_and_stability(&keyspace, &expected_variance);
         keyspace = new_keyspace;
     }
 
-    let nodes = keyspace.nodes().cloned().chain(17..256);
-    let new_keyspace = Keyspace::new(nodes, hasher, strategy).unwrap();
-    new_keyspace.assert_variance(0.13);
-    keyspace = new_keyspace;
+    for _ in 0..10 {
+        let mut nodes = keyspace.nodes.clone();
+        nodes.shift_remove_index(random::<usize>() % keyspace.nodes.len());
 
-    // for id in 4..=100 {
-    //     let snapshot = ring.clone();
+        let new_keyspace = Keyspace::new(nodes, &new_hasher, new_strategy()).unwrap();
+        new_keyspace.assert_variance_and_stability(&keyspace, &expected_variance);
+        keyspace = new_keyspace;
+    }
 
-    //     ring.remove_node(id);
-    //     ring.assert_variance();
-    //     ring.assert_data_movement(&snapshot);
-    // }
+    // let nodes = keyspace.nodes().cloned().chain(17..256);
+    // let new_keyspace = Keyspace::new(nodes, hasher, strategy).unwrap();
+    // new_keyspace.assert_variance_and_stability(&keyspace,
+    // &expected_variance); keyspace = new_keyspace;
+}
 
-    // dbg!(&keyspace.nodes);
-
-    // let mut shards_per_node = HashMap::<usize, usize>::new();
-
-    // for shard in &keyspace.hashring {
-    //     for node_idx in shard.replicas() {
-    //         *shards_per_node.entry(*node_idx).or_default() += 1;
-    //     }
-    // }
-
-    // dbg!(&shards_per_node);
-
-    // let mut hits_per_node = HashMap::<usize, usize>::new();
-
-    // for _ in 0..1_000_000 {
-    //     let idx = rand::random::<u16>();
-    //     let shard = keyspace.get_shard(ShardId(idx));
-    //     for node_idx in shard.replicas() {
-    //         *hits_per_node.entry(*node_idx).or_default() += 1;
-    //     }
-    // }
-
-    // dbg!(&hits_per_node);
-
-    // println!(
-    //     "hits min: {}, max: {}",
-    //     hits_per_node.values().min().unwrap(),
-    //     hits_per_node.values().max().unwrap(),
-    // );
-
-    panic!();
+#[test]
+fn t() {
+    let mut next_node_id = 0;
+    test_suite::<3, _, _>(
+        || DefaultHasher::new(),
+        || DefaultStrategy,
+        || {
+            next_node_id += 1;
+            next_node_id
+        },
+        ExpectedDistributionVariance(vec![(16, 0.03), (256, 0.13)]),
+    )
 }
 
 #[test]

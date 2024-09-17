32221f7c696da08828d1d6bab487a0a2435653b2
Darksome <dev@darkso.me>
2024-09-17 11:35:00 +0000

    feat: `irn cluster view` CLI subcommand

---
+++ crates/irn/src/commands/cluster/view.rs
@@ -0,0 +1,14 @@
+use anyhow::Context as _;
+
+pub(super) async fn exec(admin_api_client: &irn_admin_api::Client) -> anyhow::Result<()> {
+    let view = admin_api_client
+        .get_cluster_view()
+        .await
+        .context("irn_admin_api::Client::get_cluster_view")?;
+
+    let view = serde_json::to_string_pretty(&view).context("serde_json::to_string_pretty")?;
+
+    println!("{view}");
+
+    Ok(())
+}

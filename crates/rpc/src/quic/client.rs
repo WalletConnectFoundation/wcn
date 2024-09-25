09a21f625050701378a96c5d56f28b5b363fd544
xDarksome <dev@darkso.me>
2024-09-25 12:15:48 +0000

    fix: disable QUIC metrics (#102)

--- crates/rpc/src/quic/client.rs
+++ crates/rpc/src/quic/client.rs
@@ -1,5 +1,5 @@
 use {
-    super::{metrics::MeteredConnection, InvalidMultiaddrError},
+    super::InvalidMultiaddrError,
     crate::{
         client::{self, AnyPeer, Config, Result},
         transport::{self, BiDirectionalStream, Handshake, NoHandshake, PendingConnection},
@@ -133,7 +133,7 @@ struct ConnectionHandlerInner {
     connection_timeout: Duration,
 }
 
-type Connection = Shared<BoxFuture<'static, MeteredConnection>>;
+type Connection = Shared<BoxFuture<'static, quinn::Connection>>;
 
 fn new_connection<H: Handshake>(
     addr: SocketAddr,
@@ -168,7 +168,7 @@ fn new_connection<H: Handshake>(
                 .await
                 .map_err(|e| format!("handshake error: {e:?}"))?;
 
-            Ok(MeteredConnection::outbound(conn))
+            Ok(conn)
         }
         .map_err(backoff::Error::transient)
     };

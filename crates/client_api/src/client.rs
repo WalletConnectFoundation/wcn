36a2c9b1bc597d1333b84e0bb9ad999a25c85d2a
Darksome <dev@darkso.me>
2025-01-08 13:29:18 +0000

    feat(client_api): json RPCs

--- crates/client_api/src/client.rs
+++ crates/client_api/src/client.rs
@@ -126,7 +126,7 @@ impl Inner {
     }
 
     async fn update_cluster(&self) -> Result<(), super::Error> {
-        let update = GetCluster::send(&self.rpc_client, &AnyPeer, &())
+        let update = GetClusterV2::send(&self.rpc_client, &AnyPeer, &())
             .await
             .map_err(Error::from)?
             .map_err(Error::Api)?;
@@ -163,7 +163,8 @@ async fn updater(inner: Arc<Inner>, shutdown_rx: oneshot::Receiver<()>) {
 async fn cluster_update(inner: &Inner) {
     loop {
         let stream =
-            ClusterUpdates::send(&inner.rpc_client, &AnyPeer, &|_, rx| async move { Ok(rx) }).await;
+            ClusterUpdatesV2::send(&inner.rpc_client, &AnyPeer, &|_, rx| async move { Ok(rx) })
+                .await;
 
         let mut rx = match stream {
             Ok(rx) => rx,
@@ -259,7 +260,7 @@ impl Client {
         let timeouts = Timeouts::new()
             .with_default(config.operation_timeout)
             .with::<{ Subscribe::ID }>(None)
-            .with::<{ ClusterUpdates::ID }>(None);
+            .with::<{ ClusterUpdatesV2::ID }>(None);
 
         let rpc_client = wcn_rpc::quic::Client::new(rpc_client_config)
             .map_err(|err| Error::Other(err.to_string()))?

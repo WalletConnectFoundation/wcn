33ee8e8bab76ec32642636f7956d2a5c9ca963c1
Ivan Reshetnikov <keenn2007@gmail.com>
2025-08-23 17:10:28 +0200

    fix: clean up crypto code

--- crates/client/src/encryption.rs
+++ crates/client/src/encryption.rs
@@ -1,4 +1,5 @@
 use {
+    rand::{Rng, SeedableRng as _, rngs::StdRng},
     ring::{aead, hkdf},
     wcn_storage_api::{MapEntry, Record, RecordBorrowed, operation as op},
 };
@@ -9,20 +10,17 @@ const KEY_LEN_ENCRYPTION: usize = 32;
 
 #[derive(Debug, PartialEq, Eq, thiserror::Error, Clone)]
 pub enum Error {
-    #[error("Invalid key length")]
-    KeyLength,
-
     #[error("Invalid data")]
     Data,
 
     #[error("Invalid secret")]
     Secret,
-}
 
-impl From<ring::error::Unspecified> for Error {
-    fn from(_: ring::error::Unspecified) -> Self {
-        Self::KeyLength
-    }
+    #[error("Failed to encrypt")]
+    Encrypt,
+
+    #[error("Failed to decrypt")]
+    Decrypt,
 }
 
 #[derive(Clone)]
@@ -39,13 +37,16 @@ impl Key {
         let prk = hkdf::Salt::new(hkdf::HKDF_SHA256, KEY_SALT).extract(secret);
 
         let mut encryption_key = [0; KEY_LEN_ENCRYPTION];
-        prk.expand(&[INFO_ENCRYPTION_KEY], &aead::CHACHA20_POLY1305)?
-            .fill(&mut encryption_key)?;
 
-        let key = aead::LessSafeKey::new(aead::UnboundKey::new(
-            &aead::CHACHA20_POLY1305,
-            &encryption_key,
-        )?);
+        // Both unwraps are safe as the length everywhere is guaranteed to be
+        // compatible.
+        prk.expand(&[INFO_ENCRYPTION_KEY], &aead::CHACHA20_POLY1305)
+            .unwrap()
+            .fill(&mut encryption_key)
+            .unwrap();
+
+        let key = aead::UnboundKey::new(&aead::CHACHA20_POLY1305, &encryption_key).unwrap();
+        let key = aead::LessSafeKey::new(key);
 
         Ok(Self { key })
     }
@@ -56,7 +57,7 @@ impl Key {
         }
 
         // Generate a unique nonce.
-        let nonce = generate_nonce();
+        let nonce: [u8; aead::NONCE_LEN] = StdRng::from_rng(&mut rand::rng()).random();
 
         // Compute the total data length for `nonce + serialized data + tag`.
         let mut out = Vec::with_capacity(aead::NONCE_LEN + data.len() + aead::MAX_TAG_LEN);
@@ -68,11 +69,14 @@ impl Key {
         out.extend_from_slice(data);
 
         // Encrypt the data.
-        let tag = self.key.seal_in_place_separate_tag(
-            aead::Nonce::assume_unique_for_key(nonce),
-            aead::Aad::empty(),
-            &mut out[aead::NONCE_LEN..],
-        )?;
+        let tag = self
+            .key
+            .seal_in_place_separate_tag(
+                aead::Nonce::assume_unique_for_key(nonce),
+                aead::Aad::empty(),
+                &mut out[aead::NONCE_LEN..],
+            )
+            .map_err(|_| Error::Encrypt)?;
 
         // Write tag.
         out.extend_from_slice(tag.as_ref());
@@ -86,29 +90,15 @@ impl Key {
         } else if data.len() < aead::NONCE_LEN + aead::MAX_TAG_LEN + 1 {
             Err(Error::Data)
         } else {
-            Ok(self.key.open_in_place(
-                aead::Nonce::try_assume_unique_for_key(&data[..aead::NONCE_LEN])?,
-                aead::Aad::empty(),
-                &mut data[aead::NONCE_LEN..],
-            )?)
+            // Safe unwrap as nonce length is guaranteed to be correct.
+            let nonce = aead::Nonce::try_assume_unique_for_key(&data[..aead::NONCE_LEN]).unwrap();
+
+            Ok(self
+                .key
+                .open_in_place(nonce, aead::Aad::empty(), &mut data[aead::NONCE_LEN..])
+                .map_err(|_| Error::Decrypt)?)
         }
     }
-}
-
-fn generate_nonce() -> [u8; aead::NONCE_LEN] {
-    let timestamp = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .expect("time went backwards")
-        .as_micros();
-
-    // N.B. `u64` can fit 584554 average Gregorian years in microseconds.
-    let timestamp = (timestamp as u64).to_be_bytes();
-    let padding = rand::random::<u32>().to_be_bytes();
-
-    let mut data = [0; aead::NONCE_LEN];
-    data[..timestamp.len()].copy_from_slice(&timestamp);
-    data[timestamp.len()..].copy_from_slice(&padding);
-    data
 }
 
 pub(super) fn decrypt_output(output: &mut op::Output, key: &Key) -> Result<(), Error> {
